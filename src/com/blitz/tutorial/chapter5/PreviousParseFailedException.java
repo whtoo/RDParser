19dd20da6a322cf47e5c03f7862cfb1d58e82f7f
Blitz.Whtoo <whtoo@qq.com>
2020-05-10 08:54:12 +0800

    1. 增加javacc编译命令。

--- src/com/blitz/tutorial/chapter5/PreviousParseFailedException.java
+++ src/com/blitz/tutorial/chapter5/PreviousParseFailedException.java
@@ -2,6 +2,11 @@ package com.blitz.tutorial.chapter5;
 
 
 public class PreviousParseFailedException extends RecognitionException {
+    /**
+     *
+     */
+    private static final long serialVersionUID = -5921807809497762103L;
+
     public PreviousParseFailedException() {
         super(null);
     }
--- src/com/blitz/tutorial/chapter5/RecognitionException.java
+++ src/com/blitz/tutorial/chapter5/RecognitionException.java
@@ -1,6 +1,11 @@
 package com.blitz.tutorial.chapter5;
 
 public abstract class RecognitionException extends Exception {
+    /**
+     *
+     */
+    private static final long serialVersionUID = -2150305854545032689L;
+
     public RecognitionException(String msg) {
 	    super(msg);
     }
